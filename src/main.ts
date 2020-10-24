49c2aaf4b22e4bdf60986116a219a863ec9b2833
Rhio Kim <rhio.kim@gmail.com>
2020-10-24 10:49:16 +0900

    feat: add remove logs

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,17 @@ async function run(): Promise<void> {
     const files: string[] = core.getInput('files').split(' ')
 
     core.debug(new Date().toTimeString())
+
     for await (const file of files) {
+      core.debug(`remove ${file}`)
+
       unlink(file, error => {
         if (error) {
           throw error
         }
       })
     }
+
     core.debug(new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
